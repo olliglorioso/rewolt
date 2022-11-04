38c73df819decb84abb40edf50c8eec822b42645
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2022-11-04 20:33:35 +0000

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import TextField from '@mui/material/TextField';
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
