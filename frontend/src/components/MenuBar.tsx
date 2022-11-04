e1ae03301280ff6491828c13dbf4f8bb06f2ef16
denis <denis.kuznetsov@aalto.fi>
2022-11-04 23:56:33 +0200

    navbar base

---
+++ frontend/src/components/MenuBar.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+
+const MenuBar = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        Hi
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default MenuBar
