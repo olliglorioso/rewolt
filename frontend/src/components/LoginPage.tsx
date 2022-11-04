aa1f3b5309638966d1aca736bdc3dcaef4fd10ed
denis <denis.kuznetsov@aalto.fi>
2022-11-04 22:31:12 +0200

    login page base

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
 import TextField from '@mui/material/TextField';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login page</h1>
+      <TextField />
+      <TextField />
+    </div>
+  )
+}
+
+export default LoginPage
