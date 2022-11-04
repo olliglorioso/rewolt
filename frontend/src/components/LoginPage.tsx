d0bdbe5a324502e52aef23422775672f2a004df5
denis <denis.kuznetsov@aalto.fi>
2022-11-04 23:37:10 +0200

    login vxxx

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import { Box, Button, Typography } from '@mui/material';
+import { Button, TextField, Typography, Box } from "@mui/material";
+import React from "react";
 
 const LoginPage = () => {
-  return (
-    <Box sx={{
-      display: "flex",
-      flexDirection: "row",
-      width: "full",
-      height: "screen",
-      justifyContent: "center"
-    }}>
-      <Box sx={
-        {
-          display: "flex",
-          flexDirection: "column",
-          maxWidth: 400,
-          maxHeight: 500,
-          bgcolor: "info.main",
-          borderRadius: 5,
-          p: 10,
-          justifyContent: "center",
-          gap: 5
-        }
-      }>
-        <Typography variant='h3'>Login</Typography>
-        <div>
-          <p>Username</p>
-          <TextField label="Username" variant='outlined'/>
-        </div>
-        <div>
-          <p>Password</p>
-          <TextField label="Password" variant="outlined"/>
-        </div>
-        <Button variant='contained'>Login</Button>
-      </Box>
+  return <Box sx={{
+    display: "flex",
+    flexDirection: "row",
+    width: "full",
+    height: "screen",
+    justifyContent: "center"
+  }}>
+    <Box sx={
+      {
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: 400,
+        maxHeight: 500,
+        borderRadius: 5,
+        boxShadow: 3,
+        p: 10,
+        justifyContent: "center",
+        gap: 3
+      }
+    }>
+      <Typography variant="h3">Login</Typography>
+      <div>
+        <Typography variant="body1">Email</Typography>
+        <TextField variant="outlined" label="Email" fullWidth/>
+      </div>
+      <div>
+        <Typography variant="body1">Password</Typography>
+        <TextField variant="outlined" label="Username" fullWidth/>
+      </div>
+      <Button variant="contained" color="info">Login now</Button>
     </Box>
-  )
+  </Box>
 }
 
 export default LoginPage
