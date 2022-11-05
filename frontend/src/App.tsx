898bc0754bf849d49f4035eb9321d3cc0a877187
Samu Toljamo <samu.toljamo@avanio.com>
2022-11-05 15:13:23 +0200

    drop off

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,15 @@ import {
   redirect,
 } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MenuBar from "./components/MenuBar";
 import OrderPage from "./components/OrderPage";
 import { Box, Toolbar } from "@mui/material";
 import RegisterPage from "./components/RegisterPage";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import NanPage from "./components/404Page";
 import SuccessOrderPage from "./components/SuccesOrder";
+import { setToken } from "./redux/store";
 
 interface StateT {
   token: string;
@@ -21,9 +22,13 @@ interface StateT {
 
 export default function App() {
   let token: any = useSelector((state: StateT) => state.token);
+  const dispatch = useDispatch();
   if (!token || token === "") {
     token = localStorage.getItem("token") ? localStorage.getItem("token") : "";
   }
+  useEffect(() => {
+    dispatch(setToken(token));
+  }, [token, dispatch]);
 
   const tokenExists = token !== "" && token !== undefined;
   return (
