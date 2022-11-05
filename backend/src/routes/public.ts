8fdcbb0a84935ea70fa55257f91487a6f7cf50e8
Samu Toljamo <samu.toljamo@avanio.com>
2022-11-05 17:19:48 +0200

    err handler

--- backend/src/routes/public.ts
+++ backend/src/routes/public.ts
@@ -14,7 +14,6 @@ interface LoginPayload {
 
 router.post("/api/login", async (req, res) => {
   // login
-  console.log(req.body)
   if(!req.body || !req.body.email || !req.body.password) {
     return res.status(400).json({
       message: "Missing email or password",
