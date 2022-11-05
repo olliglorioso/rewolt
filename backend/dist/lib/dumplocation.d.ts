4899d168a663722bb14a25ae4c57688fa4821a01
Olli <olli.glorioso@gmail.com>
2022-11-05 20:07:42 +0200

    backend rebuild

---
+++ backend/dist/lib/dumplocation.d.ts
@@ -0,0 +1,3 @@
+declare const axios: any;
+declare const BASE_URL = "https://dump-location-uxsxrlriaa-lm.a.run.app";
+declare const getDumpLocation: (lat: string, lon: string, type: string) => Promise<any>;
